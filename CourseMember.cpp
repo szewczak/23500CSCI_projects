1df7e0bada6e77cd1cb4c149f4d6f81f12eebaa6
NSz <nick.szewczak@gmail.com>
2019-02-08 12:09:01 -0500

    Task

--- CourseMember.cpp
+++ CourseMember.cpp
@@ -1,8 +1,27 @@
 /**
 // @ file CourseMember.cpp
-// Project 1A
+// Project 1B
 // 
 // Created by Nick Szewczak on 1/31/19
+// Part B of Project1 will let you practice a bit more with writing simple classes and
+// become accustomed to working with multiple classes and multiple source files, while
+// applying the new concept of Inheritance.
+// 
+// In part A you wrote the !!CourseMember!! !class!. Every CourseMember has a first
+// name, last name and ID by which they can be identified. Everyone in this course is a
+// CurseMember, however there are different *roles* a CourseMember may take. We
+// have Students, TeachingAssistants and Instructors. These are all CourseMembers
+// (with a first name, last name and ID), but they also have other attributes specific to
+// their role.
+// For part B of Project1 you will write !!3 classes!!:
+// - Student
+// - TeachingAssistant
+// - Instructor
+// Both Students and Instructors are CourseMembers. A TeachingAssistant is not only a
+// CourseMember but also a Student. Thus the !!inheritance structure!! will be as follows:
+// - Student will be a derived class of CourseMember
+// - TeachingAssistant will be a derived class of Student
+// - Instructor will be a derived class of CourseMember
 */ 
  
 
